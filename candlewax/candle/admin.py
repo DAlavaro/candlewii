63311a70b316111f8227e92123f3e3c3576d8752
dd <d.alavaro@gmail.com>
2023-03-22 13:40:16 +0000

    2023/03/22 Admin bar

--- candlewax/candle/admin.py
+++ candlewax/candle/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 
-from .models import Candle, Info, About, Delivery, Reviews
+from .models import Candle, Info, About, Delivery, Reviews, Category
 
-admin.site.register(Candle)
+class CandleAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'title',
+                    'time_create',
+                    'photo',
+                    'is_published',
+                    'price')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'price')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(Candle, CandleAdmin)
+
+admin.site.register(Category, CategoryAdmin)
 
 admin.site.register(Info)
 
--- candlewax/candle/models.py
+++ candlewax/candle/models.py
@@ -11,7 +11,7 @@ class Candle(models.Model):
     weight = models.CharField(max_length=255, verbose_name='Вес')
     amount = models.CharField(max_length=255, verbose_name='Кол-во в коробке')
     price = models.FloatField(verbose_name='Цена')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время редактирования')
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
@@ -27,16 +27,22 @@ class Candle(models.Model):
     class Meta:
         verbose_name = 'Таблица товаров'
         verbose_name_plural = 'Таблица товаров'
+        ordering = ['time_create', 'title']
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('catalog', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
 
 
 class Info(models.Model):
