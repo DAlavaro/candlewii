a578f0e0035fa9b49a44581c52872c8266d85fa6
dd <d.alavaro@gmail.com>
2023-03-31 18:02:14 +0100

    #14. Формы, связанные с моделями. Пользовательские валидаторы | Django уроки

--- candlewax/candle/urls.py
+++ candlewax/candle/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', CandleHome.as_view(), name='home'),
     path('info/', info, name='info'),
     path('about/', about, name='about'),
     path('delivery/', delivery, name='delivery'),
     path('reviews/', reviews, name='reviews'),
     path('login/', login, name='login'),
     path('buy/<int:buy_id>/', show_buy, name='buy'),
-    path('catalog/<slug:cat_slug>', show_catalog, name='catalog'),
-    path('product/<slug:prod_slug>', show_product, name='product'),
+    path('catalog/<slug:cat_slug>', CandleCatalog.as_view(), name='catalog'),
+    path('product/<slug:prod_slug>', ShowPost.as_view(), name='product'),
 ]
--- candlewax/candle/views.py
+++ candlewax/candle/views.py
@@ -1,19 +1,36 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DeleteView, CreateView
 
 from .forms import *
-from .models import Candle, Info, About, Delivery, Reviews, Category
+from .models import *
 
+# класс главной страницы index.html
+class CandleHome(ListView):
+    model = Candle
+    template_name = 'candle/index.html'
+    context_object_name = 'posts'
 
-def index(request):
-    posts = Candle.objects.all()
-    context = {
-        'posts': posts,
-        'title': 'Главная страница',
-        'cat_selected': 0,
-    }
-    return render(request, 'candle/index.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
 
+    # Функция для отображения только опубликованных записей на главной странице
+    def get_queryset(self):
+        return Candle.objects.filter(is_published=True)
+
+# Функция главной страницы
+# def index(request):
+#     posts = Candle.objects.all()
+#     context = {
+#         'posts': posts,
+#         'title': 'Главная страница',
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'candle/index.html', context=context)
 
 def info(request):
     posts = Info.objects.all()
@@ -42,6 +59,21 @@ def delivery(request):
     return render(request, 'candle/delivery.html', context=context)
 
 
+
+
+# Class для добавления нового отзыва
+# class AddReviews(CreateView):
+#     form_class = AddPostForm
+#     template_name = 'candle/reviews.html'
+#     context_object_name = 'posts'
+#     success_url = reverse_lazy('home')
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = 'Отзывы и предложения'
+#         return context
+
+# Функция для добавления нового отзыва
 def reviews(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
@@ -69,33 +101,66 @@ def show_buy(request, buy_id):
     return HttpResponse(f'Отображение корзины с id = {buy_id}')
 
 
-def show_product(request, prod_slug):
-    post = get_object_or_404(Candle, slug=prod_slug)
+class ShowPost(DeleteView):
+    model = Candle
+    template_name = 'candle/post.html'
+    slug_url_kwarg = 'prod_slug'
+    #pk_url_kward = 'pk'
+    context_object_name = 'post'
 
-    context = {
-        'post': post,
-        'title': post.title,
-        'cat_selected': post.cat_id
-    }
-
-    return render(request, 'candle/post.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        return context
 
 
-def show_catalog(request, cat_slug):
-    # cat = Category.objects.get(slug=cat_slug)
-    posts = Candle.objects.filter(cat__slug=cat_slug)
 
-    print(len(posts))
-    if len(posts) == 0:
-        raise Http404()
+# Функция для отображения информации по товару
+# def show_product(request, prod_slug):
+#     post = get_object_or_404(Candle, slug=prod_slug)
+#
+#     context = {
+#         'post': post,
+#         'title': post.title,
+#         'cat_selected': post.cat_id
+#     }
+#
+#     return render(request, 'candle/post.html', context=context)
 
-    context = {
-        'posts': posts,
-        'title': 'Отображение по каталогу',
-        'cat_selected': cat_slug,
-    }
+# Class отображения списка по категориям
+class CandleCatalog(ListView):
+    model = Candle
+    template_name = 'candle/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
-    return render(request, 'candle/index.html', context=context)
+    # Функция для отображения выбранных и опубликованных записей
+    def get_queryset(self):
+        return Candle.objects.filter(cat__slug=self.kwargs['cat_slug'],
+                                     is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория -' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+# Функция отображения списка по категориям
+# def show_catalog(request, cat_slug):
+#     # cat = Category.objects.get(slug=cat_slug)
+#     posts = Candle.objects.filter(cat__slug=cat_slug)
+#
+#     print(len(posts))
+#     if len(posts) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'posts': posts,
+#         'title': 'Отображение по каталогу',
+#         'cat_selected': cat_slug,
+#     }
+#
+#     return render(request, 'candle/index.html', context=context)
 
 
 def pageNotFound(request, exception):
